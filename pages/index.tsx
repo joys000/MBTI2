b06903b2b4b5362c9a3ec014e5cd338857cc7fd6
joys000 <momlego5510@gmail.com>
2025-06-25 16:39:11 +0900

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,9 @@
+// pages/index.tsx
 export default function Home() {
   return (
     <main style={{ textAlign: "center", padding: "100px" }}>
-      <p>성격 유형 검사를 시작해보세요!</p>
-      <a href="/start" style={{ fontSize: "18px", color: "blue" }}>
-        시작하기
-      </a>
+      <h1>MBTI 테스트에 오신 걸 환영합니다</h1>
+      <a href="/start" style={{ fontSize: "18px", color: "blue" }}>시작하기</a>
     </main>
   );
 }
