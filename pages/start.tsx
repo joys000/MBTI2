8b7e95b84fa99e44cdc0034196ee4f8881f56c4b
joys000 <momlego5510@gmail.com>
2025-06-25 16:24:05 +0900

    Update start.tsx

--- pages/start.tsx
+++ pages/start.tsx
@@ -1,3 +1,9 @@
 export default function Start() {
-  return <div>테스트 시작 페이지</div>;
+  return (
+    <div style={{ padding: "50px", textAlign: "center" }}>
+      <h2>성별을 선택해주세요</h2>
+      <button style={{ margin: "10px" }}>여자</button>
+      <button style={{ margin: "10px" }}>남자</button>
+    </div>
+  );
 }
